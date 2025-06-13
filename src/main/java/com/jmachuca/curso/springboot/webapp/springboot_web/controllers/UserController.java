55e37224f43e72edc5f743096119be9c22d0610d
JFelipex3 <jfelipe.machuca@gmail.com>
2025-06-12 20:02:40 -0400

    Inicio Proyecto Curso SpringBoot Udemy

---
+++ src/main/java/com/jmachuca/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.jmachuca.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jmachuca.curso.springboot.webapp.springboot_web.models.User;
+
+@Controller
+public class UserController {
+    
+    @GetMapping("/details")
+    public String details(Model model) {
+
+        User user = new User("Jhonnatan", "Machuca");
+
+        model.addAttribute("title", "Hola Mundo");
+        model.addAttribute("user", user);
+
+        return "details";
+    }
+}
