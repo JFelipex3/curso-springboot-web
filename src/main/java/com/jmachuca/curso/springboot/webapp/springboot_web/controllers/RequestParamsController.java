16a2e4278b2ca930fd1a245a498455f5e5b7d0aa
JFelipex3 <jfelipe.machuca@gmail.com>
2025-06-14 13:47:02 -0400

    Ejemplo URL para metodos

--- src/main/java/com/jmachuca/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/jmachuca/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -11,6 +11,7 @@ import com.jmachuca.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 @RequestMapping("/api/params")
 public class RequestParamsController {
 
+    //http://localhost:8080/api/params/foo?message=Hola%20que%20tal
     @GetMapping("/foo")
     public ParamDto foo(@RequestParam(required = false, defaultValue = "Sin mensaje") String message) {
 
@@ -21,6 +22,7 @@ public class RequestParamsController {
         return param;
     }
 
+    // http://localhost:8080/api/params/fooParamCustom?mensaje=Hola%20que%20tal
     @GetMapping("/fooParamCustom")
     public ParamDto fooParamCustom(@RequestParam(required = false, defaultValue = "Sin mensaje", name = "mensaje") String message) {
 
