5ed2d2b99e734be7cc0e8bc57f25ba3383d659de
JFelipex3 <jfelipe.machuca@gmail.com>
2025-06-14 13:01:04 -0400

    each en Thymeleaf

--- src/main/java/com/jmachuca/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/jmachuca/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,7 +1,13 @@
 package com.jmachuca.curso.springboot.webapp.springboot_web.controllers;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.jmachuca.curso.springboot.webapp.springboot_web.models.User;
@@ -19,5 +25,23 @@ public class UserController {
         model.addAttribute("user", user);
 
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+
+        List<User> users = new ArrayList<>();
+        
+        users = Arrays.asList(
+            new User("Carlos", "Gomez"),
+            new User("Ana", "Perez", "ana.perez@correo.com"),
+            new User("Luis", "Fernandez"),
+            new User("Maria", "Lopez", "maria.lopez@correo.com")
+        );
+
+        model.addAttribute("title", "Listado de Usuarios");
+        model.addAttribute("users", users);
+
+        return "list";
     }
 }
