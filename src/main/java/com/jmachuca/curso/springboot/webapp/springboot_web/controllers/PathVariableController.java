51421b589e832d60ee058323a89c71de3b7b6a47
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-06-17 17:59:27 -0400

    Usar valores de properties con anotación @Value e Injección como parametro del valor

--- src/main/java/com/jmachuca/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/jmachuca/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.jmachuca.curso.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,18 @@ import com.jmachuca.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.code}")
+    private Integer code;
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
     
     // http://localhost:8080/api/var/baz/mesa
     @GetMapping("/baz/{message}")
@@ -51,6 +64,34 @@ public class PathVariableController {
     public User create(@RequestBody User user) {
 
         return user;
-    } 
+    }
+
+    // http://localhost:8080/api/var/values
+    @GetMapping("/values")
+    public Map<String, Object> getValues() {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("code", code);
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+
+        return json;
+    }
+
+    // http://localhost:8080/api/var/valuesInject
+    // Injectar valores de application.properties
+    @GetMapping("/valuesInject")
+    public Map<String, Object> getValuesInject(@Value("${config.title}") String title) {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("code", code);
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        json.put("title", title);
+
+        return json;
+    }
 
 }
